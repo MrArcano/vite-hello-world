e49c9546a1a98f381f766dfb604da29d6a70a10a
MrArcano <salvatore-dario@hotmail.it>
2023-10-09 15:26:23 +0200

    [edit]

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+
+<script>
+export default {
+  name: "Hero",
+  data() {
+    return {
+      text: "Hero",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<style scoped>
+</style>
